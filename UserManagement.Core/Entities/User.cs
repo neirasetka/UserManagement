ea94600b73f84fcd57eeaa0187acd15194f03cd0
Neira Setka <neira.setka@edu.fit.ba>
2022-06-08 16:17:27 +0200

    Add project files.

---
+++ UserManagement.Core/Entities/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagement.Common.Enums;
+
+namespace UserManagement.Core.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Username { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+
+        public string Email { get; set; }
+
+        public Status UserStatus { get; set; } = Status.Active;
+
+        public List<Permission> Permissions { get; set; } = new List<Permission>();
+    }
+}
---
+++ UserManagement.Database/UserManagementDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagement.Core.Entities;
+
+namespace UserManagement.Database
+{
+    public class UserManagementDbContext:DbContext
+    {
+        public UserManagementDbContext(DbContextOptions<UserManagementDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+    }
+}
