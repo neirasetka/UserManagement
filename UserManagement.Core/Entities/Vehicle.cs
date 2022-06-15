8f0172d7aa03114e4ab51c33203db565decbb50f
Neira Setka <neira.setka@edu.fit.ba>
2022-06-15 14:44:27 +0200

    Added Migration

--- UserManagement.Core/Entities/Vehicle.cs
+++ UserManagement.Core/Entities/Vehicle.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace UserManagement.Core.Entities
 {
     public class Vehicle
     {
         public int Id { get; set; }
-        public string Name{ get; set; }
+        public string Name { get; set; }
         public string LicensePlate { get; set; }
         public string Manufacturer { get; set; }
         public bool IsDeleted { get; set; } = false;
-        List<Expense> Expenses { get; set; }
+        public List<Expense> Expenses { get; set; }
+        public User User { get; set; }
     }
 }
---
+++ UserManagement.Database/Migrations/20220615123251_UserVehicle.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UserManagement.Database.Migrations
+{
+    public partial class UserVehicle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Vehicles",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vehicles_UserId",
+                table: "Vehicles",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vehicles_Users_UserId",
+                table: "Vehicles",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vehicles_Users_UserId",
+                table: "Vehicles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Vehicles_UserId",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Vehicles");
+        }
+    }
+}
